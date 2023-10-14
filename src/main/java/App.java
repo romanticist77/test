931cfa465c4ec5531950b556468846dc354496a1
Kamran Akhmedov <romanticist77@gmail.com>
2023-10-14 21:01:43 +0300

    refactored

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,6 @@ public class App {
 
     public static void main(String[] args) {
         Calculator calculator = new CalculatorImpl();
-        int a = 1;
         System.out.println(calculator.add(22, 33) == 55);
     }
 }
