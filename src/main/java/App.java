b1c6013b01f105723c648993b72a83fb3b91ae36
Kamran Akhmedov <romanticist77@gmail.com>
2023-10-14 21:35:48 +0300

    changed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ public class App {
 
     public static void main(String[] args) {
         Calculator calculator = new CalculatorImpl();
+        int a = 0;
         System.out.println(calculator.add(2, 3) == 5);
     }
 }
