64545513f4d3c8f29e3af3cb0cccec2a6d165018
Kamran Akhmedov <77003516+romanticist77@users.noreply.github.com>
2023-10-14 22:25:25 +0300

    Update App2.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ public class App {
 
     public static void main(String[] args) {
         Calculator calculator = new CalculatorImpl();
+        int b = 4;
         System.out.println(calculator.add(2, 3) == 5);
     }
 }
