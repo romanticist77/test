44077841246cebfe0613904d141f82a874b485e3
Kamran Akhmedov <romanticist77@gmail.com>
2023-10-15 17:00:56 +0300

    rebasing commit 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,6 @@ public class App {
 
     public static void main(String[] args) {
         Calculator calculator = new CalculatorImpl();
-        int b = 4;
         System.out.println(calculator.add(2, 3) == 5);
     }
 }
