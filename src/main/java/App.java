6f94719cd328469a0ee2f171cf71d8f3d9ef2e9c
Kamran Akhmedov <77003516+romanticist77@users.noreply.github.com>
2023-10-15 11:39:05 +0300

    changed remotely.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,6 @@ public class App {
     public static void main(String[] args) {
         Calculator calculator = new CalculatorImpl();
         int b = 4;
-        int с = 2;
         System.out.println(calculator.add(2, 3) == 5);
     }
 }
