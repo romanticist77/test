8267bf2dcbf9055c6ce20be33250f62ce24ec699
Kamran Akhmedov <77003516+romanticist77@users.noreply.github.com>
2023-10-15 17:02:49 +0300

    remote changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ public class App {
 
     public static void main(String[] args) {
         Calculator calculator = new CalculatorImpl();
+        int c = 4;
         System.out.println(calculator.add(2, 3) == 5);
     }
 }
